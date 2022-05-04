c4d797d0054649d1954e3d47f54410e4cfff19c4
Huyen Tran <63681821+genuhuyen@users.noreply.github.com>
2022-05-03 22:00:11 -0500

    Update GreetingController.java
    provides option to get full html text or just the body of text

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -11,19 +11,30 @@ import java.text.SimpleDateFormat;
 
 @Controller
 public class GreetingController{
-/*
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}*/
 
+	//return full html text to client
+	/*
 	@GetMapping("/greeting")
-	public ResponseEntity<String> greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS");
+	public String greeting(@RequestParam(name="client_name", required=false, defaultValue="World") String client_name, Model model) {
+		model.addAttribute("client_name", client_name);
+
+		//handling datetime
+		SimpleDateFormat formatter = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
 		Date date = new Date();
-		return new ResponseEntity<>("Hello, " + name + " it's " + formatter.format(date) + " over here!", HttpStatus.OK);
+		String date_time = formatter.format(date);
+		model.addAttribute("date_time", date_time);
+
+		return "greeting";
+	} */
+
+	//return only text to client
+	@GetMapping("/greeting")
+	public ResponseEntity<String> greeting(@RequestParam(name="client_name", required=false, defaultValue="World") String client_name, Model model) {
+		model.addAttribute("name", client_name);
+		SimpleDateFormat formatter = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+		Date date = new Date();
+		String date_time = formatter.format(date);
+		return new ResponseEntity<>("Hi " + client_name + ", it's " + date_time + " over here!", HttpStatus.OK);
 	}
 
 }
