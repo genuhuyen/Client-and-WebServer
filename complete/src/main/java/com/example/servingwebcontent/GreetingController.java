d37c635b773e690a2eb7edb39411fab9820a9f41
Huyen Tran <63681821+genuhuyen@users.noreply.github.com>
2022-05-04 16:43:34 -0500

    Update GreetingController.java

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -6,28 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 @Controller
 public class GreetingController{
 
-	//return full html text to client
-	/*
 	@GetMapping("/greeting")
+	@ResponseBody
 	public String greeting(@RequestParam(name="client_name", required=false, defaultValue="World") String client_name, Model model) {
 		model.addAttribute("client_name", client_name);
 
 		//handling datetime
-		SimpleDateFormat formatter = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date date = new Date();
 		String date_time = formatter.format(date);
 		model.addAttribute("date_time", date_time);
 
-		return "greeting";
-	} */
+		//return "greeting"; //this is our view
+		return "Hi " + client_name + ", it's " + date_time + " over here!";
+	}
 
-	//return only text to client
+	//another way to return just spring as http response!
+	/*
 	@GetMapping("/greeting")
 	public ResponseEntity<String> greeting(@RequestParam(name="client_name", required=false, defaultValue="World") String client_name, Model model) {
 		model.addAttribute("name", client_name);
@@ -35,6 +37,6 @@ public class GreetingController{
 		Date date = new Date();
 		String date_time = formatter.format(date);
 		return new ResponseEntity<>("Hi " + client_name + ", it's " + date_time + " over here!", HttpStatus.OK);
-	}
+	}*/
 
 }
